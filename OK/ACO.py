a5992340e54a43bb69e0227640559bf2a2aaea53
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2018-12-13 15:43:19 +0100

    Debugged generating random solution and little progress with pheromone matrix

--- OK/ACO.py
+++ OK/ACO.py
@@ -1,16 +1,15 @@
 from random import randint, choice, uniform
 from math import ceil
-import msvcrt as m
+from copy import deepcopy
 
-
-# HEURISTIC GLOBAL VAIRABLES
-TASKS = 50
+# HEURISTIC GLOBAL VARIABLES
+TASKS = 10
 T_MIN = 5
 T_MAX = 20
 MAINTENANCE_CHANCE = 0.3
 M2_PENALTY = 0.1
 MAINTENANCE_TIME_COEFFICIENT = 1.5
-POPULATION = 100
+POPULATION = 10
 ITERATIONS = 100
 BEST_ANTS = 20
 EVAPORATION_RATE = 0.2
@@ -31,15 +30,27 @@ MAX_MAINTENANCE = TASKS - 2
 
 class Job:
 	def __init__(self, o1, o2, i):
+		self.begin_time_m1 = None
+		self.begin_time_m2 = None
+		self.end_time_m1 = None
+		self.end_time_m2 = None
+		self.penalty = None
+		self.real_length_op2 = None
 		self.op1 = o1
 		self.op2 = o2
-		self.index = i
+		self.id = i
 
-	def set_begin_time(self, time):
-		self.begin_time = time
+	def set_begin_time_m1(self, time):
+		self.begin_time_m1 = time
 
-	def set_end_time(self, time):
-		self.end_time = time
+	def set_begin_time_m2(self, time):
+		self.begin_time_m2 = time
+
+	def set_end_time_m1(self, time):
+		self.end_time_m1 = time
+
+	def set_end_time_m2(self, time):
+		self.end_time_m2 = time
 
 	def set_penalty(self, p):
 		self.penalty = p
@@ -47,17 +58,18 @@ class Job:
 
 	def __str__(self):
 		# return f"job{self.index}"
-		return str(self.index)
+		return str(self.id)
 
 	def __repr__(self):
-		re2turn str(self)
+		return str(self)
 
 
 class Maintenance:
-	def __init__(self, time):
+	def __init__(self, time, job):
 		self.begin_time = time
 		self.length = MAINTENANCE_DURATION
 		self.end_time = time + self.length
+		self.parent_job = job.id
 
 	def __str__(self):
 		return "mtc"
@@ -69,9 +81,11 @@ class Maintenance:
 class IdleTime:
 	def __init__(self, time):
 		self.begin_time = time
+		self.end_time = None
+		self.length = None
 
 	def set_end_time(self, time):
-		self.end_time = time 
+		self.end_time = time
 		self.length = self.end_time - self.begin_time
 
 	def __str__(self):
@@ -85,7 +99,7 @@ class Ant:
 	def __init__(self, solution_m1, solution_m2):
 		self.solution_m1 = solution_m1
 		self.solution_m2 = solution_m2
-		self.score = score(self.solution_m1, self.solution_m2)
+		self.score = None
 
 	def set_score(self):
 		self.score = score(self.solution_m1, self.solution_m2)
@@ -101,7 +115,7 @@ def generate_jobs():
 	print(TASKS, file=INPUT_FILE)
 	t2_sum = 0
 
-	for i in range(TASKS):		
+	for i in range(TASKS):
 		t1 = randint(T_MIN, T_MAX)
 		t2 = randint(T_MIN, T_MAX)
 		t2_sum += t2
@@ -116,17 +130,17 @@ def generate_jobs():
 	AVERAGE_M2_TIME = ceil(t2_sum / TASKS)
 	MAINTENANCE_DURATION = ceil(MAINTENANCE_TIME_COEFFICIENT * AVERAGE_M2_TIME)
 
- 
+
 def generate_random_solution():
- 	#SOLUTION VARIABLES
+	# SOLUTION VARIABLES
 	solution_m1 = list()
 	solution_m2 = list()
 	solution_m2_with_maintenance = list()
 	maintenance_count = 0
 	idle_count = 0
 
- 	#OTHER VARIABLES
-	not_used_jobs = JOBS.copy()
+	# OTHER VARIABLES
+	not_used_jobs = deepcopy(JOBS)
 	possible_m2_jobs = list()
 	m1_time_busy = 0
 	m2_time_busy = 0
@@ -134,31 +148,32 @@ def generate_random_solution():
 	last_done = False
 	current_m2_penalty = 1
 	time = 0
-	is_idle= False
+	is_idle = False
 
 	while len(solution_m2) != len(JOBS) or m2_time_busy > 0:
 		if m1_time_busy < 1 and len(not_used_jobs) > 0:
 			if first_done:
 				solution_m1.append(job1)
-				job1.set_end_time(time)
+				job1.set_end_time_m1(time)
 				possible_m2_jobs.append(job1)
 
 			first_done = True
 			job1 = choice(not_used_jobs)
 			not_used_jobs.remove(job1)
 			m1_time_busy = job1.op1
-			job1.set_begin_time(time)
+			job1.set_begin_time_m1(time)
 
-		if len(not_used_jobs) == 0 and m1_time_busy < 1 and not(last_done):
+		if len(not_used_jobs) == 0 and m1_time_busy < 1 and not (last_done):
 			last_done = True
 			solution_m1.append(job1)
-			job1.set_end_time(time)
+			job1.set_end_time_m1(time)
 			possible_m2_jobs.append(job1)
-			# print("----M1 END----")
+		# print("----M1 END----")
 
 		if (m2_time_busy < 1) and (len(possible_m2_jobs) > 0 or current_m2_penalty > 1):
-			# random choice between normal job or maintenance. X is float between 0 and 1 and if its lower than MAINTENANCE_CHANCE algorithm is choosing maintenance
-			# program cannot do more than 1 maintenance in a row, so it will ignore random choice if current_m2_penalty == 1
+			# random choice between normal job or maintenance. X is float between 0 and 1 and if its lower than
+			# MAINTENANCE_CHANCE algorithm is choosing maintenance program cannot do more than 1 maintenance in a row,
+			# so it will ignore random choice if current_m2_penalty == 1
 			x = uniform(0, 1)
 			# print(x)
 
@@ -166,23 +181,23 @@ def generate_random_solution():
 				if is_idle:
 					idle.set_end_time(time)
 					solution_m2_with_maintenance.append(idle)
-					is_idle= False
+					is_idle = False
 
 				current_m2_penalty = 1
 				m2_time_busy = MAINTENANCE_DURATION
 				maintenance_count += 1
-				maintenance = Maintenance(time)
+				maintenance = Maintenance(time, job2)
 				solution_m2_with_maintenance.append(maintenance)
 
 			elif len(possible_m2_jobs) > 0:
 				if is_idle:
 					idle.set_end_time(time)
 					solution_m2_with_maintenance.append(idle)
-					is_idle= False
+					is_idle = False
 
 				job2 = choice(possible_m2_jobs)
-				job2.set_begin_time(time)
-				job2.set_end_time(time + job2.op2)
+				job2.set_begin_time_m2(time)
+				job2.set_end_time_m2(time + job2.op2)
 				solution_m2.append(job2)
 				solution_m2_with_maintenance.append(job2)
 				possible_m2_jobs.remove(job2)
@@ -190,11 +205,11 @@ def generate_random_solution():
 				job2.set_penalty(current_m2_penalty)
 				current_m2_penalty += M2_PENALTY
 
-			elif len(possible_m2_jobs) == 0 and x >= MAINTENANCE_CHANCE and not(is_idle):
+			elif len(possible_m2_jobs) == 0 and not is_idle:
 				idle = IdleTime(time)
 				is_idle = True
 
-		elif m2_time_busy < 1 and len(possible_m2_jobs) == 0 and current_m2_penalty == 1 and not(is_idle):
+		elif m2_time_busy < 1 and len(possible_m2_jobs) == 0 and current_m2_penalty == 1 and not is_idle:
 			idle = IdleTime(time)
 			is_idle = True
 
@@ -208,8 +223,8 @@ def generate_random_solution():
 
 
 def score(solution_m1, solution_m2):
-	m1_score = solution_m1[-1].end_time
-	m2_score = solution_m2[-1].end_time
+	m1_score = solution_m1[-1].end_time_m1
+	m2_score = solution_m2[-1].end_time_m2
 
 	return m1_score + m2_score
 
@@ -226,19 +241,19 @@ def generate_output_file(solution_m1, solution_m2):
 	print("M1: ", end="", file=OUTPUT_FILE)
 	first = False
 	for job in solution_m1:
-		if not(first):
+		if not first:
 			first = True
-			print(f"op1_{job.index}, {job.begin_time}, {job.op1}, {job.op1}", end="", file=OUTPUT_FILE)
+			print(f"op1_{job.id}, {job.begin_time_m1}, {job.op1}, {job.op1}", end="", file=OUTPUT_FILE)
 		else:
-			print(f"; op1_{job.index}, {job.begin_time}, {job.op1}, {job.op1}", end="", file=OUTPUT_FILE)
+			print(f"; op1_{job.id}, {job.begin_time_m1}, {job.op1}, {job.op1}", end="", file=OUTPUT_FILE)
 
 	print("", file=OUTPUT_FILE)
 	first = False
 	print("M2: ", end="", file=OUTPUT_FILE)
 
-	for element in solution_m2:		
+	for element in solution_m2:
 		if type(element) is Job:
-			print(f"; op1_{element.index}, {element.begin_time}, {element.op1}, {element.op1}", end="", file=OUTPUT_FILE)
+			print(f"; op1_{element.id}, {element.begin_time_m2}, {element.op1}, {element.op1}", end="", file=OUTPUT_FILE)
 
 		elif type(element) is Maintenance:
 			print(f"; maint{maintenance_count}_M2, {element.begin_time}, {element.length}", end="", file=OUTPUT_FILE)
@@ -246,7 +261,7 @@ def generate_output_file(solution_m1, solution_m2):
 			maintenance_time += element.length
 
 		elif type(element) is IdleTime:
-			if not(first):
+			if not first:
 				first = True
 				print(f"idle{idle_count}_M2, {element.begin_time}, {element.length}", end="", file=OUTPUT_FILE)
 
@@ -255,9 +270,9 @@ def generate_output_file(solution_m1, solution_m2):
 			idle_count += 1
 			idle_time += element.length
 
-	print("\n0", file=OUTPUT_FILE) 
+	print("\n0", file=OUTPUT_FILE)
 	print(str(maintenance_time), file=OUTPUT_FILE)
-	print("0", file=OUTPUT_FILE) 
+	print("0", file=OUTPUT_FILE)
 	print(str(idle_time), end="", file=OUTPUT_FILE)
 
 
@@ -280,28 +295,38 @@ def initialize_pheromone_matrix():
 	for i in range(TASKS):
 		temp = list()
 		for j in range(TASKS):
-			temp.append(None)
+			temp.append(0)
 		PHEROMONE_MATRIX_M1.append(temp)
 
-	for i in range(TASKS + MAX_MAINTENANCE):
+	for i in range(2 * TASKS):
 		temp = list()
-		for j in range(TASKS + MAX_MAINTENANCE):
-			temp.append(None)
+		for j in range(2 * TASKS):
+			temp.append(0)
 		PHEROMONE_MATRIX_M2.append(temp)
 
 
 def update_pheromone_matrix(ants):
+	global PHEROMONE_MATRIX_M1, PHEROMONE_MATRIX_M2
 	max_weight = BEST_ANTS
-	old_pheromone_matrix = PHEROMONE_MATRIX.copy()
+	# old_pheromone_matrix = PHEROMONE_MATRIX.copy()
 
-	for row in PHEROMONE_MATRIX:
-		for cell in row:
-			for ant in ants:
+	for ant in ants:
+		for i in range(len(ant.solution_m1) - 1):
+			row = ant.solution_m1[i].id
+			column = ant.solution_m1[i + 1].id
+			PHEROMONE_MATRIX_M1[row][column] += 1
+
+		for i in range(len(ant.solution_m2) - 1):
+			row = ant.solution_m2[i].id
+			column = ant.solution_m2[i + 1].id
+			PHEROMONE_MATRIX_M1[row][column] += 1
 
 
 
 generate_jobs()
-solution_m1, solution_m2_with_maintenance = generate_random_solution()
-generate_output_file(solution_m1, solution_m2_with_maintenance)
+# solution_m1, solution_m2_with_maintenance = generate_random_solution()
+# generate_output_file(solution_m1, solution_m2_with_maintenance)
 start_population()
-get_x_best()
+# # get_x_best()
+initialize_pheromone_matrix()
+# update_pheromone_matrix(ANTS)
